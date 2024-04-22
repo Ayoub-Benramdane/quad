a7bfe67d00e31793e695f189f1eec0180d2b7ad4
Ayoub Benramdane <145620173+Ayoub-Benramdane@users.noreply.github.com>
2024-04-22 21:33:22 +0100

    Add files via upload

---
+++ QuadA.go
@@ -0,0 +1,24 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func QuadA(x, y int) {
+	if x <= 0 || y <= 0 {
+		return
+	}
+
+	for i := 0; i < y; i++ {
+		for j := 0; j < x; j++ {
+			if (j == 0 || j == x-1) && (i == 0 || i == y-1) {
+				z01.PrintRune('o')
+			} else if j == 0 || j == x-1 {
+				z01.PrintRune('|')
+			} else if i != 0 && i != y-1 {
+				z01.PrintRune(' ')
+			} else {
+				z01.PrintRune('-')
+			}
+		}
+		z01.PrintRune('\n')
+	}
+}
